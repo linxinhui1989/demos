40d2651767d2648e982b9278e52202ba31ff075e
linxinhui1989 <984275894@qq.com>
2019-08-20 15:09:48 +0800

    对于ajax封装

---
+++ 09.php
@@ -0,0 +1,5 @@
+<?php 
+	$userName = $_GET["username"];
+	$pwd = $_GET["pwd"];
+	echo $userName." ==> ".$pwd;
+ ?>
---
+++ 09_2.php
@@ -0,0 +1,5 @@
+<?php 
+	$userName = $_POST["username"];
+	$pwd = $_POST["pwd"];
+	echo $userName." ==> ".$pwd." ---> 这边是post请求";
+ ?>
