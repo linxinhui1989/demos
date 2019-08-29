037743d173195fe1e48c42d6b0f1fcdd221ba15c
linxinhui1989 <984275894@qq.com>
2019-08-29 15:18:17 +0800

    js基础

---
+++ mj.js
@@ -0,0 +1,12 @@
+var $$ = {};
+/**
+ * [show Is used to retrieve user input data]
+ * @return {[string]} [The result of the final processing]
+ */
+$$.show = function(){}
+/**
+ * [sum description]
+ * @param  {[type]} nums... [description]
+ * @return {[type]}         [description]
+ */
+$$.sum = function(nums...){}
